fa6388a9dd067f30b05ff228449a89557a02cf5b
peter-lyr <llydrp.ldp@gmail.com>
2024-04-29 09:14:08 +0800

    copy.relative

--- lua/dp_copy.lua
+++ lua/dp_copy.lua
@@ -56,22 +56,22 @@ function M.cur_root()
 end
 
 require 'which-key'.register {
-  ['<leader>y'] = { name = 'dp_copy', },
+  ['<leader>y'] = { name = 'copy', },
   ['<leader>yw'] = { function() M.copy_cwd() end, 'copy cwd to clipboard', mode = { 'n', 'v', }, silent = true, },
 }
 
 require 'which-key'.register {
-  ['<leader>yf'] = { name = 'dp_copy: absolute path', },
-  ['<leader>yff'] = { function() M.full_name() end, 'full name', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>yft'] = { function() M.full_tail() end, 'full tail', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>yfh'] = { function() M.full_head() end, 'full head', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>yf'] = { name = 'copy.absolute', },
+  ['<leader>yff'] = { function() M.full_name() end, 'copy full name to clipboard', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>yft'] = { function() M.full_tail() end, 'copy full tail to clipboard', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>yfh'] = { function() M.full_head() end, 'copy full head to clipboard', mode = { 'n', 'v', }, silent = true, },
 }
 
 require 'which-key'.register {
-  ['<leader>yr'] = { name = 'dp_copy: relative path', },
-  ['<leader>yrr'] = { function() M.rela_name() end, 'rela name', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>yrh'] = { function() M.rela_head() end, 'rela head', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>yrc'] = { function() M.cur_root() end, 'telescope cur root', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>yr'] = { name = 'copy.relative', },
+  ['<leader>yrr'] = { function() M.rela_name() end, 'copy rela name to clipboard', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>yrh'] = { function() M.rela_head() end, 'copy rela head to clipboard', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>yrc'] = { function() M.cur_root() end, 'copy CurRoot to clipboard', mode = { 'n', 'v', }, silent = true, },
 }
 
 return M
