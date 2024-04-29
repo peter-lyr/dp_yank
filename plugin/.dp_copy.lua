3c111284b3a38fb33faee22eea300a7ed820ddc9
peter-lyr <llydrp.ldp@gmail.com>
2024-04-30 00:41:26 +0800

    yank

--- plugin/.dp_copy.lua
+++
@@ -1,6 +0,0 @@
-require 'dp_copy'
-local t = vim.fn.reltimefloat(vim.fn.reltime(StartTime))
-if not StartTimeList then
-  StartTimeList = {}
-end
-StartTimeList[#StartTimeList+1] = string.format("`%.3f` [%s]", t, vim.fn.fnamemodify(debug.getinfo(1)['source'], ':t:r'))
