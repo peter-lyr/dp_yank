653b51656f891e6959ba44832e25f6f7dd14959d
peter-lyr <llydrp.ldp@gmail.com>
2024-05-06 23:38:51 +0800

    <leader>yk yank

--- lua/dp_yank.lua
+++ lua/dp_yank.lua
@@ -11,7 +11,7 @@ if B.check_plugins {
   return
 end
 
-function M._copy(content)
+function M._yank(content)
   vim.fn.setreg('+', content)
   B.notify_info(content)
 end
@@ -25,53 +25,53 @@ function M._get_rela_name()
 end
 
 function M.full_name()
-  M._copy(M._get_full_name())
+  M._yank(M._get_full_name())
 end
 
 function M.full_head()
-  M._copy(vim.fn.fnamemodify(M._get_full_name(), ':h'))
+  M._yank(vim.fn.fnamemodify(M._get_full_name(), ':h'))
 end
 
 function M.full_tail()
-  M._copy(vim.fn.fnamemodify(M._get_full_name(), ':t'))
+  M._yank(vim.fn.fnamemodify(M._get_full_name(), ':t'))
 end
 
-function M.copy_cwd()
-  M._copy(vim.loop.cwd())
+function M.yank_cwd()
+  M._yank(vim.loop.cwd())
 end
 
 function M.rela_name()
-  M._copy(M._get_rela_name())
+  M._yank(M._get_rela_name())
 end
 
 function M.rela_head()
-  M._copy(vim.fn.fnamemodify(M._get_rela_name(), ':h'))
+  M._yank(vim.fn.fnamemodify(M._get_rela_name(), ':h'))
 end
 
 function M.cur_root()
   local dir = CurRoot[B.get_proj_root()]
   if dir then
-    M._copy(dir)
+    M._yank(dir)
   end
 end
 
 require 'which-key'.register {
-  ['<leader>y'] = { name = 'copy', },
-  ['<leader>yw'] = { function() M.copy_cwd() end, 'copy cwd to clipboard', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>yk'] = { name = 'yank', },
+  ['<leader>ykw'] = { function() M.yank_cwd() end, 'yank cwd to clipboard', mode = { 'n', 'v', }, silent = true, },
 }
 
 require 'which-key'.register {
-  ['<leader>yf'] = { name = 'copy.absolute', },
-  ['<leader>yff'] = { function() M.full_name() end, 'copy full name to clipboard', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>yft'] = { function() M.full_tail() end, 'copy full tail to clipboard', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>yfh'] = { function() M.full_head() end, 'copy full head to clipboard', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>ykf'] = { name = 'yank.absolute', },
+  ['<leader>ykff'] = { function() M.full_name() end, 'yank full name to clipboard', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>ykft'] = { function() M.full_tail() end, 'yank full tail to clipboard', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>ykfh'] = { function() M.full_head() end, 'yank full head to clipboard', mode = { 'n', 'v', }, silent = true, },
 }
 
 require 'which-key'.register {
-  ['<leader>yr'] = { name = 'copy.relative', },
-  ['<leader>yrr'] = { function() M.rela_name() end, 'copy rela name to clipboard', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>yrh'] = { function() M.rela_head() end, 'copy rela head to clipboard', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>yrc'] = { function() M.cur_root() end, 'copy CurRoot to clipboard', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>ykr'] = { name = 'yank.relative', },
+  ['<leader>ykrr'] = { function() M.rela_name() end, 'yank rela name to clipboard', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>ykrh'] = { function() M.rela_head() end, 'yank rela head to clipboard', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>ykrc'] = { function() M.cur_root() end, 'yank CurRoot to clipboard', mode = { 'n', 'v', }, silent = true, },
 }
 
 return M
