7e92291dccbc286a4bf08113ef91f2184080d800
peter-lyr <llydrp.ldp@gmail.com>
2024-05-07 22:55:22 +0800

    <leader>y yank

--- lua/dp_yank.lua
+++ lua/dp_yank.lua
@@ -55,23 +55,25 @@ function M.cur_root()
   end
 end
 
-require 'which-key'.register {
-  ['<leader>yk'] = { name = 'yank', },
-  ['<leader>ykw'] = { function() M.yank_cwd() end, 'yank cwd to clipboard', mode = { 'n', 'v', }, silent = true, },
-}
+function M._map()
+  require 'which-key'.register {
+    ['<leader>y'] = { name = 'yank', },
+    ['<leader>yw'] = { function() M.yank_cwd() end, 'yank cwd to clipboard', mode = { 'n', 'v', }, silent = true, },
+  }
 
-require 'which-key'.register {
-  ['<leader>ykf'] = { name = 'yank.absolute', },
-  ['<leader>ykff'] = { function() M.full_name() end, 'yank full name to clipboard', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>ykft'] = { function() M.full_tail() end, 'yank full tail to clipboard', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>ykfh'] = { function() M.full_head() end, 'yank full head to clipboard', mode = { 'n', 'v', }, silent = true, },
-}
+  require 'which-key'.register {
+    ['<leader>yf'] = { name = 'yank.absolute', },
+    ['<leader>yff'] = { function() M.full_name() end, 'yank full name to clipboard', mode = { 'n', 'v', }, silent = true, },
+    ['<leader>yft'] = { function() M.full_tail() end, 'yank full tail to clipboard', mode = { 'n', 'v', }, silent = true, },
+    ['<leader>yfh'] = { function() M.full_head() end, 'yank full head to clipboard', mode = { 'n', 'v', }, silent = true, },
+  }
 
-require 'which-key'.register {
-  ['<leader>ykr'] = { name = 'yank.relative', },
-  ['<leader>ykrr'] = { function() M.rela_name() end, 'yank rela name to clipboard', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>ykrh'] = { function() M.rela_head() end, 'yank rela head to clipboard', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>ykrc'] = { function() M.cur_root() end, 'yank CurRoot to clipboard', mode = { 'n', 'v', }, silent = true, },
-}
+  require 'which-key'.register {
+    ['<leader>yr'] = { name = 'yank.relative', },
+    ['<leader>yrr'] = { function() M.rela_name() end, 'yank rela name to clipboard', mode = { 'n', 'v', }, silent = true, },
+    ['<leader>yrh'] = { function() M.rela_head() end, 'yank rela head to clipboard', mode = { 'n', 'v', }, silent = true, },
+    ['<leader>yrc'] = { function() M.cur_root() end, 'yank CurRoot to clipboard', mode = { 'n', 'v', }, silent = true, },
+  }
+end
 
 return M
